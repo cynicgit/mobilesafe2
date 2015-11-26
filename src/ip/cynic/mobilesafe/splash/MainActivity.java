753232258beafff4bee913aa1f4935a314b2b7da
cynic <957160415@qq.com>
2015-11-26 09:50:16 +0800

    手机卫士splash

---
+++ src/ip/cynic/mobilesafe/splash/MainActivity.java
@@ -0,0 +1,16 @@
+package ip.cynic.mobilesafe.splash;
+
+import ip.cynic.mobilesafe.R;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+
+}
